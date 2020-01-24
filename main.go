6277d14b89cfb61b8d2350e34839eab8d7e0ca70
giou-k <georgiok@ceid.upatras.gr>
2020-01-24 13:48:12 +0200

    Add bootstrap of world x and its cities.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/cosmos/alien-invasion/world"
 	"log"
 )
 
@@ -11,18 +12,23 @@ func main() {
 
 	var (
 		alienNum int
-		//m world.Map
+		m world.Map
 	)
 
 	// Max number of aliens must be the total number of cities.
-	flag.IntVar(&alienNum, "n", 2, "number of aliens. default: 3")
+	flag.IntVar(&alienNum, "n", 3, "number of aliens. default: 3")
 	flag.Parse()
 
-	if alienNum == 0 || alienNum > 2 {
+	if alienNum == 0 || alienNum > 3 {
 		log.Fatal("Invalid number of aliens.The number of aliens can't be 0 or greater than the number of cities.")
 	}
 
 	// Firstly bootstrap the world.
+	err := m.Bootstrap()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	// Secondly input the aliens randomly.
 
--- world/bootstrap.go
+++ world/bootstrap.go
@@ -1,5 +1,12 @@
 package world
 
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strings"
+)
+
 type Map struct {
 	Cities map[string]*City
 	Aliens map[int]*Alien
@@ -8,13 +15,55 @@ type Map struct {
 type City struct {
 	Uid int
 	Name string
-	North bool
-	East bool
-	South bool
-	West bool
+	Directions map[string]string
 	Alien *Alien
 }
 
-func (m *Map) bootstrap()  {
+// Bootstrap creates the world X and its cities based on the input file worldxin.txt.
+func (m *Map) Bootstrap() error {
+	file, err := os.Open("./worldxin.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
+	m.Cities = make(map[string]*City)
+
+	scanner := bufio.NewScanner(file)
+	cityUid := 0
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+		s := strings.Split(currentLine, " ")
+
+		// If there is an empty line just continue.
+		if len(s) == 1 {
+			continue
+		}
+
+		// Assume that the line of file must start with the name of the city.
+		city := &City{
+			Uid:  cityUid,
+			Name: s[0],
+		}
+		city.Directions = make(map[string]string)
+
+		// Check if city has directions to other cities.(should we assume that it has, even though they are not in the
+		// input file?)
+		if len(s) > 1 {
+			for _, v := range s[1:] {
+				directionTo := strings.Split(v, "=")
+				if len(directionTo) != 2 {
+					return errors.New("directions for city: " + m.Cities[s[0]].Name + "in file worldxin.txt have" +
+						" wrong format")
+				}
+				city.Directions[directionTo[0]] = directionTo[1]
+			}
+
+			m.Cities[city.Name] = city
+			cityUid++
+		}
+
+	}
+
+	return nil
 }
